5caed36be615772df30c90c0bf956f167e2aaba4
orphen92 <orphen92300@gmail.com>
2023-11-11 21:58:06 +0100

    update eslint rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
@@ -19,9 +15,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main>
-                fsdfsdfsdfs
-            </main>
+            <main>fsdfsdfsdfs</main>
         </>
     );
 }
